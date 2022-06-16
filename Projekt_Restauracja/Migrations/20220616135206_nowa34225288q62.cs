bdaa0d7f4100ad0989b3cfc9df742e02a0f2daf3
Jakub Feliński <80295@student.pb.edu.pl>
2022-06-16 18:42:07 +0200

    Poprawiony commit?

---
+++ Projekt_Restauracja/Migrations/20220616135206_nowa34225288q62.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Projekt_Restauracja.Migrations
+{
+    public partial class nowa34225288q62 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Restaurant",
+                table: "Restaurant");
+
+            migrationBuilder.RenameTable(
+                name: "Restaurant",
+                newName: "Restaurants");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RestaurantId",
+                table: "Dish",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Restaurants",
+                table: "Restaurants",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dish_RestaurantId",
+                table: "Dish",
+                column: "RestaurantId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dish_Restaurants_RestaurantId",
+                table: "Dish",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dish_Restaurants_RestaurantId",
+                table: "Dish");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Dish_RestaurantId",
+                table: "Dish");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Restaurants",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "RestaurantId",
+                table: "Dish");
+
+            migrationBuilder.RenameTable(
+                name: "Restaurants",
+                newName: "Restaurant");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Restaurant",
+                table: "Restaurant",
+                column: "Id");
+        }
+    }
+}
