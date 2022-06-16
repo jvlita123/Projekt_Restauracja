4092fe57334b3b3559a26c5288c7ba51081d6f3a
Jakub Feliński <80295@student.pb.edu.pl>
2022-06-16 19:37:51 +0200

    Poprawiony user z dodanym id (bo blad)

--- Projekt_Restauracja/Models/User.cs
+++ Projekt_Restauracja/Models/User.cs
@@ -8,7 +8,8 @@ namespace Projekt_Restauracja
 {
     public class User
     {
-            
+        
+        public int Id { get; set; }
         public string email { get; set; }
 
         public DateTime? DateOfBirth {get; set;}
